308f6cd5e49111ab17330a3abe6e97edb48e3996
Richard Banyi <rbanyi@me.com>
2020-10-26 22:42:17 +0100

    Shadow components; Don't show Posts and Projects in the homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,82 +1,97 @@
-require(`dotenv`).config({
-    path: `.env`,
+require("dotenv").config({
+    path: ".env",
 })
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
     siteMetadata: {
-        siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+        siteTitleAlt: "Richban Pages",
+        siteTitle: "RICHBAN",
+        siteHeadline: "Empty",
+        siteUrl: "http://richban.live",
+        siteDescription: "Good stuff",
+        siteLanguage: "Eng",
+        siteImage: "No site.png",
+        author: "richban"
     },
     plugins: [
         {
-            resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+            resolve: "@lekoarts/gatsby-theme-minimal-blog",
             // See the theme's README for all available options
             options: {
                 navigation: [
                     {
-                        title: `About`,
-                        slug: `/about`,
+                        title: "About",
+                        slug: "/about",
                     },
                 ],
                 externalLinks: [
                     {
-                        name: `Twitter`,
-                        url: `https://twitter.com/lekoarts_de`,
+                        name: "Notion",
+                        url: "https://www.notion.so/",
                     },
                     {
-                        name: `Instagram`,
-                        url: `https://www.instagram.com/lekoarts.de/`,
+                        name: "Github",
+                        url: "https://www.github.com/richban",
+                    },
+                    {
+                        name: "Twitter",
+                        url: "https://twitter.com/richban_",
+                    },
+                    {
+                        name: "LinkedIn",
+                        url: "https://www.linkedin.com/in/richbanyi/",
                     },
                 ],
             },
         },
         {
-            resolve: `gatsby-plugin-typescript`,
+            resolve: "gatsby-plugin-typescript",
             options: {
                 isTSX: true, // defaults to false
-                jsxPragma: `jsx`, // defaults to "React"
+                jsxPragma: "jsx", // defaults to "React"
                 allExtensions: true, // defaults to false
             },
         },
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: "gatsby-plugin-google-analytics",
             options: {
                 trackingId: process.env.GOOGLE_ANALYTICS_ID,
             },
         },
-        `gatsby-plugin-sitemap`,
+        "gatsby-plugin-sitemap",
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: "gatsby-plugin-manifest",
             options: {
-                name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-                short_name: `minimal-blog`,
-                description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-                start_url: `/`,
-                background_color: `#fff`,
-                theme_color: `#6B46C1`,
-                display: `standalone`,
+                name: "minimal-blog - @lekoarts/gatsby-theme-minimal-blog",
+                short_name: "minimal-blog",
+                description: "Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.",
+                start_url: "/",
+                background_color: "#fff",
+                theme_color: "#6B46C1",
+                display: "standalone",
                 icons: [
                     {
-                        src: `/android-chrome-192x192.png`,
-                        sizes: `192x192`,
-                        type: `image/png`,
+                        src: "/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
                     },
                     {
-                        src: `/android-chrome-512x512.png`,
-                        sizes: `512x512`,
-                        type: `image/png`,
+                        src: "/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
                     },
                 ],
             },
         },
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-netlify`,
+        "gatsby-plugin-offline",
+        "gatsby-plugin-netlify",
         shouldAnalyseBundle && {
-            resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+            resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
             options: {
-                analyzerMode: `static`,
-                reportFilename: `_bundle.html`,
+                analyzerMode: "static",
+                reportFilename: "_bundle.html",
                 openAnalyzer: false,
             },
         },
