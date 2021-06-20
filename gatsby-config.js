e2945545a627e513d9942e8396a2b834af0c5b31
richban <6967088+richban@users.noreply.github.com>
2021-06-20 18:24:00 +0200

    Removed unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,26 +102,5 @@ module.exports = {
         openAnalyzer: false,
       },
     },
-    // {
-    //   resolve: `gatsby-transformer-remark`,
-    //   options: {
-    //     plugins: [
-    //       {
-    //         resolve: `gatsby-remark-highlight-code`,
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-mdx`,
-    //   options: {
-    //     extensions: [".mdx", ".md"],
-    //     gatsbyRemarkPlugins: [
-    //       {
-    //         resolve: `gatsby-remark-highlight-code`,
-    //       },
-    //     ],
-    //   },
-    // },
   ].filter(Boolean),
 };
