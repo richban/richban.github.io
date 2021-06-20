93642417a5a7459a94c8608e7a96299db0b239e0
richban <6967088+richban@users.noreply.github.com>
2021-06-20 18:39:35 +0200

    Fix Latest Posts trailing //

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 import Title from "@lekoarts/gatsby-theme-minimal-blog/src/components/title"
 import Listing from "@lekoarts/gatsby-theme-minimal-blog/src/components/listing"
-import List from "@lekoarts/gatsby-theme-minimal-blog/src/components/list"
 import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
 import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata"
 import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes"
@@ -37,12 +36,11 @@ const Homepage = ({ posts }: PostsProps) => {
       <h1 sx={visuallyHidden}>{siteTitle}</h1>
       <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
         <Hero />
-        <p />
       </section>
-      // <Title text="Latest Posts">
-      //   <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
-      // </Title>
-      // <Listing posts={posts} showTags={false} />
+      <Title text="Latest Posts">
+        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
+      </Title>
+      <Listing posts={posts} showTags={false} />
     </Layout>
   )
 }
